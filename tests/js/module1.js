59f170182399332403b0b9dcabd128cb52d3f60b
Mike Pennisi <mike@mikepennisi.com>
2014-09-25 15:42:23 -0400

    Fix unit tests

--- tests/js/module1.js
+++ tests/js/module1.js
@@ -1,10 +1,10 @@
 define([ 'module1' ], function( module1 ) {
 
-	test( 'log calls ', function() {
+	test( 'log calls ', function( assert ) {
 		assert.ok( module1.name === 'module1' );
 	});
 
-	test( 'init method is present ', function() {
+	test( 'init method is present ', function( assert ) {
 		assert.ok( typeof module1.init === 'function' );
 	});
 
