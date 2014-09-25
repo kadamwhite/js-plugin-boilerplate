c81b0a2bf4d6ec112ccd7cfd52a8067400c2b93d
Mike Pennisi <mike@mikepennisi.com>
2014-09-25 15:42:23 -0400

    Fix unit tests

--- tests/js/module2.js
+++ tests/js/module2.js
@@ -1,6 +1,6 @@
 define([ 'module2' ], function( module2 ) {
 
-	test( '.name property was set correctly', function() {
+	test( '.name property was set correctly', function( assert ) {
 		assert.ok( module2.name === 'module2' );
 	});
 });
--- tests/tests.js
+++ tests/tests.js
@@ -1,25 +1,29 @@
 /* global requirejs:false */
 // Prevent automatic test execution
-QUnit.config.autostart = true;
-
-// Configure Require to look for modules in our main /js/src directory
-requirejs.config({
-	baseUrl: '../js/src'
-});
+QUnit.config.autostart = false;
 
 var testBaseDir = '../../tests/js/';
 
-// Require the test files, so that we can know when they've loaded
-// (and therefore when we're ready to run QUnit)
-requirejs([
-	// Prepending all test paths with '../../tests/js' tells Require to
-	// load *these* modules from the tests directory, not from the
-	// /js/src directory specified in baseUrl above. This lets us take
-	// advantage of /js/src-relative modules within our test files,
-	// without impacting our ability to load the test files via AMD.
-	testBaseDir + 'module1',
-	testBaseDir + 'module2',
-	testBaseDir + 'util/random'
-], function(/* the test modules don't export anything */) {
-	QUnit.init();
+// Configure Require to look for modules in our main /js/src directory
+window.REQUIRE_CONFIGURATION = {
+  baseUrl: '../js/src'
+};
+
+define('app', {});
+
+requirejs(['../js/require-config'], function() {
+	// Require the test files, so that we can know when they've loaded
+	// (and therefore when we're ready to run QUnit)
+	requirejs([
+		// Prepending all test paths with '../../tests/js' tells Require to
+		// load *these* modules from the tests directory, not from the
+		// /js/src directory specified in baseUrl above. This lets us take
+		// advantage of /js/src-relative modules within our test files,
+		// without impacting our ability to load the test files via AMD.
+		testBaseDir + 'module1',
+		testBaseDir + 'module2',
+		testBaseDir + 'util/random'
+	], function(/* the test modules don't export anything */) {
+		QUnit.start();
+	});
 });
